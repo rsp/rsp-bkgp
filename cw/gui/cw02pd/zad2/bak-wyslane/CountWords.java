ace11ea8afd8cb9bf81432941dac6d716a33ee94
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-03-19 07:23:55 +0100

    add z2

---
+++ cw/gui/cw02pd/zad2/bak-wyslane/CountWords.java
@@ -0,0 +1,38 @@
+/**
+ *
+ *  @author Pocztarski Rafał S13658
+ *
+ */
+
+package Zad2;
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class CountWords {
+	
+	ArrayList<String> r = new ArrayList<>();
+	
+	CountWords(String f) {
+		HashMap<String,Integer> m = new HashMap<>();
+		try (Scanner s = new Scanner(new File(f))) {
+			s.useDelimiter("[\\p{P}\\s]+");
+			while (s.hasNext())	{
+				String w = s.next();
+				m.put(w, (m.containsKey(w)?m.get(w)+1:1));
+			}
+		} catch (Exception e) {
+			System.out.println("***");
+			System.exit(0);
+		}
+		for (String k: m.keySet())
+		    r.add(k + " " + m.get(k));
+	}
+
+	ArrayList<String> getResult() {
+		return r;
+	}
+
+}  
---
+++ cw/gui/cw02pd/zad2/bak-wyslane/Main.java
@@ -0,0 +1,23 @@
+/**
+ *
+ *  @author Pocztarski Rafał S13658
+ *
+ */
+
+package Zad2;
+
+
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    String fname = System.getProperty("user.home") + "/textforwords.txt";
+    CountWords cw = new CountWords(fname);
+    List<String> result = cw.getResult();
+    for (String wordRes : result) {
+      System.out.println(wordRes);
+    }
+  }
+}
